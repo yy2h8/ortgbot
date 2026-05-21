a672f58086611560ce6032aa32702a7f582a99dd
Ilyas Sadykov <ilyas.sadykov@gmail.com>
2026-05-21 14:40:55 +0500

    translate hardcoded strings

--- src/domain/constants/defaults.py
+++ src/domain/constants/defaults.py
@@ -1,4 +1,4 @@
 MAX_PERSONA_LENGTH = 400
 PROMPT_MESSAGE_MAX_LENGTH = 300
 INITIAL_TRENDS_THRESHOLD = 10
-DEFAULT_PERSONA = "a witty, casual group member who is socially aware and comfortable having a mild opinion"
+DEFAULT_PERSONA = "смешной, непринужденный участник группы, который социально осведомлен и комфортно выражает свое мнение; неформальный стиль общения, может использовать сарказм и шутки"
